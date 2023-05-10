f2c6913aa98cf6cf0b96853406ec7c6fe5e00aab
Zhenting Wu <s6268209tw@gmail.com>
2023-05-10 09:56:57 +0800

    Initial Commit

---
+++ prac/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  prac
+//
+//  Created by Zhengting Wu on 2023/5/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
